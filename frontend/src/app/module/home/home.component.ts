fb06f00157f2fbb82613561662da9373b4bfe6db
Francesco Cina <ufoscout@gmail.com>
2018-06-04 16:13:57 +0200

    Add create modal

--- frontend/src/app/module/home/home.component.ts
+++ frontend/src/app/module/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  constructor(private modalService: NgbModal) {}
+  
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+    }, (reason) => {
+    });
+  }
 
 }
